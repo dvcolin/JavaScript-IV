91c42d67417466e471448b4e43657dea5c68109a
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-13 11:51:06 -0700

    Created classes for Person, Instructor, and Student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,50 @@
 /// CODE here for your Lambda Classes
+class Person {
+    constructor(atts) {
+        this.name = atts.name;
+        this.age = atts.age;
+        this.location = atts.location;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person{
+    constructor(atts) {
+        super(atts);
+        this.specialty = atts.specialty;
+        this.favLanguage = atts.favLanguage;
+        this.catchPhrase = atts.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Student extends Person {
+    constructor(atts) {
+        super(atts);
+        this.previousBackground = atts.previousBackground;
+        this.className = atts.className;
+        this.favSubjects = atts.favSubjects;
+    }
+
+    listsSubjects() {
+        this.favSubjects.forEach((subject) => console.log(subject));
+    }
+
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+}
